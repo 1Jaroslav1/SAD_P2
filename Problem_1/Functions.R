5a3a4d244f9f3c0b1b3401d4ce07e5988847230a
p-karbownik <baciar112@gmail.com>
2023-01-21 13:32:51 +0100

    added some tests and changed drawing charts functions

--- Problem_1/Functions.R
+++ Problem_1/Functions.R
@@ -19,10 +19,6 @@ prepare_inflation_data <- function(df, period) {
   df <- as.data.frame(sapply(df, as.double))
   df %>% select(-1) -> df
 
-  #df %>% select(-1) %>% mutate(across(everything(), ~lead(.x) - (.x))) %>% na.omit() -> tmp
-  #tmp <- as.data.frame(sapply(tmp, as.numeric))
-  #tmp %>% mutate(across(everything(), ~ . * -1)) -> tmp
-
   df <- rowMeans(df)
   df  <- data.frame(matrix(unlist(df), nrow=length(df), byrow = TRUE))
 
@@ -65,54 +61,51 @@ prepare_plot <- function(euro, nonEuro) {
 
 draw_stacking_data <- function(no_euro_zone_data, euro_zone_data, title) {
   merged_df <- merge(no_euro_zone_data, euro_zone_data, by = "Period")
-  colnames(merged_df)[2] <- "Euro zone"
-  colnames(merged_df)[3] <- "No euro zone"
+  colnames(merged_df)[2] <- "Strefa euro"
+  colnames(merged_df)[3] <- "Pozostałe państwa UE"
 
-  data_frame <- pivot_longer(merged_df, -Period, names_to = "Country", values_to = "InflationMonthly")
+  data_frame <- pivot_longer(merged_df, -Period, names_to = "Grupa", values_to = "InflationMonthly")
 
-  ggplot(data_frame, aes(x = Period, y = InflationMonthly, group = Country, color = Country)) +
+  ggplot(data_frame, aes(x = Period, y = InflationMonthly, group = Grupa, color = Grupa)) +
     # scale_color_viridis(discrete = TRUE, option = "H") +
-    geom_line(aes(color = factor(Country)), size = 2) +
+    geom_line(aes(color = factor(Grupa)), size = 2) +
     geom_point(shape = 21, color = "black", fill = "black", size = 1.5) +
     scale_x_date(date_labels = "%Y-%m", date_minor_breaks = "1 month") +
     guides(fill = guide_legend(title = NULL)) +
-    xlab("Date") +
-    ylab("Inflation [%]") +
+    xlab("Miesiąc") +
+    ylab("Wartość inflacji [%]") +
     ggtitle(title) +
     theme_minimal() +
     theme(
-      plot.title=element_text(size = 30, hjust=0.5, vjust=0.5, face='bold', margin = margin(20, 0, 20, 0)),
-      axis.title = element_text(size=20,face="bold"),
+      plot.title=element_text(size = 20, hjust=0.5, vjust=0.5, margin = margin(20, 0, 20, 0)),
+      axis.title = element_text(size=14),
       axis.title.x = element_text(margin = margin(20, 0, 20, 0)),
       axis.title.y = element_text(margin = margin(0, 20, 0, 20)),
       axis.text = element_text(
         size=15,
         face=3
       ),
-      legend.text = element_text(size = 15),
-      legend.title = element_text(size = 20),
+      legend.text = element_text(size = 12),
+      legend.title = element_text(size = 14),
     )
 }
 
 draw_histogram <- function(df, title, column_name, mean, sd, color) {
   cat(column_name, ": ", "mean - ", mean, ', standard deviation = ', sd, '\n')
   ggplot(df) +
-    geom_histogram(binwidth = 0.1, aes(x = get(column_name), y = after_stat(density)), fill = paste(color, '1'), color = color) +
-    xlab("Monthly inflation delta") +
-    xlim(-1.5, 2.5) +
-    ylab("Density") +
-    ylim(0,2) +
+    geom_histogram(binwidth = 0.1, aes(x = get(column_name), y = after_stat(count)), fill = paste(color, '1'), color = color) +
+    xlab("Wartość inflacji") +
+    ylab("Ilość wystąpień") +
     theme_light() +
     ggtitle(title) +
     theme(
-      plot.title=element_text(size = 25, hjust=0.5, vjust=0.5, face='bold', margin = margin(20, 0, 20, 0)),
-      axis.title = element_text(size = 20),
+      plot.title=element_text(size = 20, hjust=0.5, vjust=0.5, margin = margin(20, 0, 20, 0)),
+      axis.title = element_text(size = 14),
       axis.title.x = element_text(margin = margin(20, 0, 20, 0)),
       axis.title.y = element_text(margin = margin(0, 20, 0, 20)),
       axis.text = element_text(
         size = 15,
         face = 3
       )
-    ) +
-    stat_function(fun = dnorm, args = list(mean = mean, sd = sd), col = 'black')
+    )
 }
